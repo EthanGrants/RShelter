e73b9c6475cc2498720c57429e2f251c1baab032
EthanGrants <105686621+EthanGrants@users.noreply.github.com>
2024-01-20 13:38:39 -0800

    Button styling

--- src/Layout.js
+++ src/Layout.js
@@ -1,29 +1,48 @@
 import { Outlet, Link } from "react-router-dom";
 
+const buttonContainerStyle = {
+    textAlign: "center", // Center align the buttons
+  };
+
+
+const buttonStyle = {
+    padding: "10px 50px",
+    margin: "2px",
+    backgroundColor: "#9a08d7",
+    color: "#fff",
+    textDecoration: "none",
+    cursor: "pointer",
+    border: "50px",
+    border: "2px solid #5bd4ef", // Border color
+    lineHeight: "2",
+  };
+
+  
+
 const Layout = () => {
   return (
+    <div style={buttonContainerStyle}>
     <>
-    <Link to={"./"}>
+    <Link to={"./"} style={buttonStyle}>
     Home
   </Link>
 
-  <Link to={"./weather"}>
+  <Link to={"./weather"} style={buttonStyle}>
     Weather
   </Link>
 
-  <Link to={"./transportation"}>
+  <Link to={"./transportation"} style={buttonStyle}>
     Transportation
   </Link>
-  <Link to={"./maps"}>
+  <Link to={"./maps"} style={buttonStyle}>
     Maps
   </Link>
-  <Link to={"./resources"}>
+  <Link to={"./resources"} style={buttonStyle}>
     Resources
   </Link>
-
-
-      <Outlet />
+   <Outlet />
     </>
+    </div>
   )
 };
 
