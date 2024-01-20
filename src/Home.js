10d2ffca7076a1540ecf82c3c5bc484455ecbcac
EthanGrants <105686621+EthanGrants@users.noreply.github.com>
2024-01-20 15:29:06 -0800

    Changes to Home Page

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,67 @@
 import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 
 const homeStyle = {
     backgroundColor: '#bde6ec',
     width: "100%",
-    // height: "100vh",
+    color: "#fff",
+    textShadow: "0 4px 8px rgba(155, 8, 215, 0.5)",
+    fontSize: "30px",
     
 }
+
+
+const linkContainerStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 1fr)", // 2 columns with equal width
+    gap: "300px", // Adjust the gap between links
+    textAlign: "center",
+    marginTop: "100px",
+    // Center
+    margin: "auto",
+  };
+
+  const linkStyle = {
+    padding: "10px",
+  backgroundColor: "#fff",
+  border: "2px solid #5bd4ef",
+  boxShadow: "0 4px 8px rgba(155, 8, 215, 0.5)",
+  borderRadius: "5px", // Border radius for rounded corners
+  textDecoration: "none",
+  color: "#000", // Text color
+  fontWeight: "bold",
+  textShadow: "none",
+  
+  }
+  
 
 function Home() {
     return (
       <div style={homeStyle}>
-        <h2>Home Page</h2>
-        {/* Add content specific to the Home page */}
+        <h2>R'Shelter</h2>
+
+        <div style = {linkContainerStyle}>
+  <Link to={"./weather"} style={linkStyle}>
+    Weather
+    <div style={{ fontSize: "14px", color: "#555" }}>Check the safety of your area</div>
+  </Link>
+
+  <Link to={"./transportation"} style={linkStyle}>
+    Transportation
+    <div style={{ fontSize: "14px", color: "#555" }}>Find publc transportation for Riverside</div>
+  </Link>
+
+  <Link to={"./maps"} style={linkStyle}>
+    Maps
+    <img src="./HomelessShelter.jpg" alt="Shelter" />
+    <div style={{ fontSize: "14px", color: "#555" }}>Find a local shelter near you</div>
+  </Link>
+
+  <Link to={"./resources"} style={linkStyle}>
+    Resources
+    <div style={{ fontSize: "14px", color: "#555" }}>Available emergency services</div>
+  </Link>
+      </div>
       </div>
     );
   }
