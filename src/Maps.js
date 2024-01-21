b3454c3dedf0f89bfe869ddf46051ee962621108
Benjamin Lim <blim038@ucr.edu>
2024-01-20 17:03:24 -0800

    Adding maps.js

--- src/Maps.js
+++ src/Maps.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Map from './GoogleMap';
+
 
 function Maps() {
     return <div>
@@ -6,4 +8,17 @@ function Maps() {
     </div>
 }
 
-export default Maps;
+
+
+function App() {
+  return (
+    <div style={{ height: '500px', width: '100%' }}>
+      <Map
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
+    </div>
+  );
+}
+
+export default App;
