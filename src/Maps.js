22ed3b78873cf3985600398f7d0d8894f96053db
Benjamin Lim <blim038@ucr.edu>
2024-01-20 18:44:03 -0800

    Google maps working

--- src/Maps.js
+++ src/Maps.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import Map from './GoogleMap';
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
 
-function Maps() {
-    return <div>
-        <h2> Maps Page </h2>
-    </div>
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDt_et4e5arhJOr4b-DGh3gBJW961RwHnc"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <div style = {{
+
+        width: "100%",
+        height: "100vh",
+        background:"red",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "end"
+
+    }}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+      </div>
+  ) : <></>
 }
 
-
-
-function App() {
-  return (
-    <div style={{ height: '500px', width: '100%' }}>
-      <Map
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
-    </div>
-  );
-}
-
-export default App;
+export default React.memo(MyComponent)
